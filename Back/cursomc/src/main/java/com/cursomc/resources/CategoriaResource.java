3b84b977cb83c5558e6906288213b387374b93a8
Bruno Santos <brunocruz55171@gmail.com>
2022-04-11 13:13:51 -0300

    DTO's: CategoriaDto no findAll

--- Back/cursomc/src/main/java/com/cursomc/resources/CategoriaResource.java
+++ Back/cursomc/src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.cursomc.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.dto.CategoriaDto;
 import com.cursomc.services.CategoriaService;
 
 @RestController
@@ -23,10 +25,12 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Categoria>> find() {
-		
+	public ResponseEntity<List<CategoriaDto>> find() {
 		List<Categoria> categorias = service.findAll();
-		return ResponseEntity.ok().body(categorias);
+		List<CategoriaDto> categoriaDto = categorias.stream().map(categoria -> new CategoriaDto(categoria))
+															 .collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(categoriaDto);
 	}
 	
 	
